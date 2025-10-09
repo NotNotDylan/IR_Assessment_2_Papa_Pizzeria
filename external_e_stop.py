1ce723369428b475b920b21cb502dd371bb926ee
Not-Not-Dylan <163363019+NotNotDylan@users.noreply.github.com>
2025-10-09 17:46:33 +1100

    Made a comment

--- external_e_stop.py
+++ external_e_stop.py
@@ -1,5 +1,5 @@
 # if using pyserial for Arduino comm (ensure to install pyserial)
-import serial
+import serial # pip install pyserial
 
 class ExternalEStop:
     """Handles input from a physical emergency stop button (via Arduino) asynchronously."""
